f93cb3f24a6e0e28885b7af6f17c9ae3425b0da5
andrew <boessena@bc.edu>
2024-09-03 23:09:31 -0400

    smem

--- kernels/smem.cu
+++ kernels/smem.cu
@@ -25,4 +25,8 @@ void __global__ gemm_smem(int M, int N, int K, const float *A, const float *B, f
   A += blockIdx.x * BLOCKSIZE * K;
   B += blockIdx.y * BLOCKSIZE;
   C += blockIdx.x * BLOCKSIZE * N + blockIdx.y * BLOCKSIZE;
+
+  for (int blkId = 0; blkId < K; blk += BLOCKSIZE) {
+
+  }
 }
