ec169f4d014f057c4179b2910d6c2a2ccfbfc82f
andrew <boessena@bc.edu>
2024-09-15 12:06:29 -0400

    smem kernel

--- kernels/smem.cu
+++ kernels/smem.cu
@@ -25,8 +25,30 @@ void __global__ gemm_smem(int M, int N, int K, const float *A, const float *B, f
   A += blockIdx.x * BLOCKSIZE * K;
   B += blockIdx.y * BLOCKSIZE;
   C += blockIdx.x * BLOCKSIZE * N + blockIdx.y * BLOCKSIZE;
+  
+  float tmp = 0.0;
 
   for (int blkId = 0; blkId < K; blkId += BLOCKSIZE) {
+    // Load block data into shared memory
+    As[threadRow * BLOCKSIZE + threadCol] = A[threadRow * K + threadCol];
+    Bs[threadRow * BLOCKSIZE + threadCol] = B[threadRow * N + threadCol];
+  
+    // wait for SMEM to be filled
+    __sycthread();
 
+    // advance pointers onto next chunk
+    A += BLOCKSIZE;
+    B += BLOCKSIZE * N;
+  
+    // execute the dotproduct on the currently cached block
+    for (int dotIdx = 0; dotIdx < BLOCKSIZE; ++dotIdx) {
+      tmp += As[threadRow * BLOCKSIZE + dotIdx] *
+              Bs[dotIdx * BLOCKSIZE + threadCol];
+    }
+    // need to sync again at the end, to avoid faster threads
+    // fetching the next block into the cache before slower threads are done
+    __syncthreads();
   }
+  C[threadRow * N + threadCol] =
+      alpha * tmp + beta * C[threadRow * N + threadCol];
 }
